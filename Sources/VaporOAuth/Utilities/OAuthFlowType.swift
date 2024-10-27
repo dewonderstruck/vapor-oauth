8420f8c5f11630d798de87f549f4b46f61dbf874
vamsii777 <vamsi.madduluri@icloud.com>
2024-10-28 00:18:31 +0530

    Deprecate Implicit Grant flow
    The Implicit Grant flow has been deprecated due to security concerns.  The Authorization Code flow with PKCE is now the recommended alternative.  This change updates the `OAuthFlowType` enum to reflect this deprecation and adds a warning message to the `implicit` case.

--- Sources/VaporOAuth/Utilities/OAuthFlowType.swift
+++ Sources/VaporOAuth/Utilities/OAuthFlowType.swift
@@ -1,6 +1,9 @@
 public enum OAuthFlowType: String, Sendable {
     case authorization = "authorization_code"
+    
+    @available(*, deprecated, message: "The Implicit Grant flow is deprecated and not recommended for security reasons. Use Authorization Code flow with PKCE instead.")
     case implicit = "implicit"
+    
     case password = "password"
     case clientCredentials = "client_credentials"
     case refresh = "refresh_token"
